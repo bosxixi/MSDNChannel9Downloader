957291855d9735dddeccdbcacbf46f1f81a6ddc4
bosxixi <bosxixi@outlook.com>
2016-08-27 13:37:14 +0800

    获取标题和链接

--- MSDNChannel9Downloader/Program.cs
+++ MSDNChannel9Downloader/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using HtmlAgilityPack;
+using HapCss;
 
 namespace MSDNChannel9Downloader
 {
@@ -10,6 +14,46 @@ namespace MSDNChannel9Downloader
     {
         static void Main(string[] args)
         {
+            //MainAsync().Wait();
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument dom = web.Load("https://channel9.msdn.com/Series/aspnetmonsters");
+            var videos = dom.DocumentNode.ChildNodes.QuerySelectorAll("ul.entries li a.title").Select(c => 
+            new Video
+            {
+                Title = c.InnerText.Replace("&#160;", " "),
+                Url = c.Attributes.First(a => a.Name == "href").Value
+            });
+
+            foreach (var item in videos)
+            {
+                item.ToString();
+            }
+        }
+
+        static async Task MainAsync()
+        {
+            //HttpClient client = new HttpClient();
+            //string result = await client.GetStringAsync("https://channel9.msdn.com/Series/aspnetmonsters");
+
+            //Console.WriteLine(result);
+            //HtmlAgilityPack.HtmlDocument dom = new HtmlDocument();
+
+        }
+
+        class Video
+        {
+            public string Url { get; set; }
+
+            public string Title { get; set; }
+
+            public override string ToString()
+            {
+                Console.WriteLine($"Title: {Title}");
+                Console.WriteLine($"Url: {Url}");
+                Console.WriteLine();
+                return string.Empty;
+            }
         }
     }
 }
